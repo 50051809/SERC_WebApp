e69b25a06d24f461bcf1b83a07087230f66ae0b9
50051809 <50051809@belfastmet.ac.uk>
2022-07-08 15:30:52 +0100

    add images

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://dog.ceo/dog-api/', true);
+request.open('GET', 'https://dog.ceo/api/breed/spaniel/cocker/images', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
@@ -23,6 +23,9 @@ request.onload = function () {
       const h1 = document.createElement('h1');
       h1.textContent = movie.title;
 
+      const img = document.createElement('img');
+      img.src = movie.image
+     
       const p = document.createElement('p');
       movie.description = movie.description.substring(0, 300);
       p.textContent = `${movie.description}...`;
@@ -30,6 +33,7 @@ request.onload = function () {
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
+      card.appendChild(img);
     });
   } else {
     const errorMessage = document.createElement('marquee');
