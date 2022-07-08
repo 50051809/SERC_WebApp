a1b92abc09acfd441702c9cdc03382f8e3d5e48a
50051809 <50051809@belfastmet.ac.uk>
2022-07-08 14:25:22 +0100

    adding dog ceo link

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://dog.ceo/dog-api/', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
